51153269c0a752ac3c97081cb510b47562f7ab1b
Bas <Bassievdploeg@hotmail.com>
2016-09-16 11:23:57 +0200

    win states

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,33 @@
-/**
- * Created by denni on 14-9-2016.
- */
+var playerWinOptions = [];
+var aiWinOptions = [];
+
+//Player
+//Diagonal
+playerWinOptions[0] = [1, 1, 1, 0, 0, 0, 0, 0, 0];
+playerWinOptions[1] = [0, 0, 0, 1, 1, 1, 0, 0, 0];
+playerWinOptions[2] = [0, 0, 0, 0, 0, 0, 1, 1, 1];
+//Oblique
+playerWinOptions[3] = [1, 0, 0, 0, 1, 0, 0, 0, 1];
+playerWinOptions[5] = [0, 0, 1, 0, 1, 0, 1, 0, 0];
+//Vertical
+playerWinOptions[6] = [1, 0, 0, 1, 0, 0, 1, 0, 0];
+playerWinOptions[7] = [0, 1, 0, 0, 1, 0, 0, 1, 0];
+playerWinOptions[8] = [0, 0, 1, 0, 0, 1, 0, 0, 1];
+
+//AI
+//Diagonal
+aiWinOptions[0] = [1, 1, 1, 0, 0, 0, 0, 0, 0];
+aiWinOptions[1] = [0, 0, 0, 1, 1, 1, 0, 0, 0];
+aiWinOptions[2] = [0, 0, 0, 0, 0, 0, 1, 1, 1];
+//Oblique
+aiWinOptions[3] = [1, 0, 0, 0, 1, 0, 0, 0, 1];
+aiWinOptions[5] = [0, 0, 1, 0, 1, 0, 1, 0, 0];
+//Vertical
+aiWinOptions[6] = [1, 0, 0, 1, 0, 0, 1, 0, 0];
+aiWinOptions[7] = [0, 1, 0, 0, 1, 0, 0, 1, 0];
+aiWinOptions[8] = [0, 0, 1, 0, 0, 1, 0, 0, 1];
+
+$(document).ready(function () {
+    console.log(playerWinOptions);
+    console.log(aiWinOptions);
+});
