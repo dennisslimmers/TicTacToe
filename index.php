699d40d1ffd6bfb9d6dafba300466727bb8f7a59
blockswallower <dennisslimmers@live.nl>
2016-09-16 10:54:27 +0200

    Added playing board

--- index.php
+++ index.php
@@ -2,8 +2,25 @@
     <html lang="nl">
     <head>
         <title>TicTacToe</title>
+        <style>
+            button {
+                height: 60px;
+                width: 60px;
+            }
+        </style>
     </head>
     <body>
         <h1>TicTacToe!</h1>
+        <button></button>
+        <button></button>
+        <button></button>
+        <br>
+        <button></button>
+        <button></button>
+        <button></button>
+        <br>
+        <button></button>
+        <button></button>
+        <button></button>
     </body>
 </html>
