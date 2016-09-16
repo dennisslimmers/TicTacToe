282a11ed1bfeabcd1b938419bb70d15e2d65a3b5
blockswallower <dennisslimmers@live.nl>
2016-09-17 00:54:48 +0200

    Added A.I win options to ajax class

--- ajax.php
+++ ajax.php
@@ -1,12 +1,30 @@
 <?php
 
 class ajax {
+    private $aiWinOptions = [];
+
     public function __construct($post) {
 	    $this->pushBoardState($post);
 
         /*
-         * TODO: create A.I win options (Same as in script.js)
+         * Diagonal
          */
+        $this->aiWinOptions[0] = [1, 1, 1, 0, 0, 0, 0, 0, 0];
+        $this->aiWinOptions[1] = [0, 0, 0, 1, 1, 1, 0, 0, 0];
+        $this->aiWinOptions[2] = [0, 0, 0, 0, 0, 0, 1, 1, 1];
+
+        /*
+         * Oblique
+         */
+        $this->aiWinOptions[3] = [1, 0, 0, 0, 1, 0, 0, 0, 1];
+        $this->aiWinOptions[4] = [0, 0, 1, 0, 1, 0, 1, 0, 0];
+
+        /*
+         * Vertical
+         */
+        $this->aiWinOptions[5] = [1, 0, 0, 1, 0, 0, 1, 0, 0];
+        $this->aiWinOptions[6] = [0, 1, 0, 0, 1, 0, 0, 1, 0];
+        $this->aiWinOptions[7] = [0, 0, 1, 0, 0, 1, 0, 0, 1];
     }
 
     public function pushBoardState($post) {
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -8,18 +8,18 @@ playerWinOptions[1] = [0, 0, 0, 1, 1, 1, 0, 0, 0];
 playerWinOptions[2] = [0, 0, 0, 0, 0, 0, 1, 1, 1];
 //Oblique
 playerWinOptions[3] = [1, 0, 0, 0, 1, 0, 0, 0, 1];
-playerWinOptions[5] = [0, 0, 1, 0, 1, 0, 1, 0, 0];
+playerWinOptions[4] = [0, 0, 1, 0, 1, 0, 1, 0, 0];
 //Vertical
-playerWinOptions[6] = [1, 0, 0, 1, 0, 0, 1, 0, 0];
-playerWinOptions[7] = [0, 1, 0, 0, 1, 0, 0, 1, 0];
-playerWinOptions[8] = [0, 0, 1, 0, 0, 1, 0, 0, 1];
+playerWinOptions[5] = [1, 0, 0, 1, 0, 0, 1, 0, 0];
+playerWinOptions[6] = [0, 1, 0, 0, 1, 0, 0, 1, 0];
+playerWinOptions[7] = [0, 0, 1, 0, 0, 1, 0, 0, 1];
 
 function pushBoardState(id, e) {
     boardState[id - 1] = 1;
 
     // TODO: Do we even need this if we aren't working with sessions?
     $("#buttonId").val(id);
-    
+
     var page = 'passAjax.php';
 
     $.ajax({
